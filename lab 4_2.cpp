dc7f48735b5562b2b8d98ba541258f16de1b0de4
Steven <thundersteve77@gmail.com>
2016-05-14 15:34:31 -0600

    extranio me eestuvo lanzando errrores

---
+++ lab 4_2.cpp
@@ -0,0 +1,24 @@
+#include <iomanip>
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void leeCadena();
+void pedirNumber();
+
+int main(int argc, char*argv[]){
+	char* numeroAzar=NULL;
+	char* numeroUsua=NULL;
+	numeros=new char[4];
+
+	cout<<"ingrese numero a adivinar ";
+	cin.getline(nombre,4);
+
+	cout<<"numero ingresado es: "<<nombre<<endl;
+	
+	
+	return 0;
+}
+	
+
---
+++ lab4_2.cpp
@@ -0,0 +1,28 @@
+#include <iomanip>
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void leeCadena(char* ,int);
+void pedirNumber();
+
+int main(int argc, char*argv[]){
+	char* numeroAzar=NULL;
+	char* numeroUsua=NULL;
+
+	numeros=new char[4];
+
+	cout<<"ingrese numero ";
+	cin.getline(nombre,4);
+
+	cout<<"numero ingresado es: "<<nombre<<endl;
+	
+	
+	return 0;
+}
+	
+void leeCadena(char* cadena ,int size){
+	cout<<"ingrese numero ";
+	cin.getline(cadena,size);
+}
